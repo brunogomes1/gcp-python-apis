8d85442cf7db0af1c675939c58b31329b63eea9d
brunogomes1 <80266185+brunogomes1@users.noreply.github.com>
2021-03-23 17:35:25 -0300

    using more vars
    using more vars

--- cloudsql-api/turn_on/main.py
+++ cloudsql-api/turn_on/main.py
@@ -1,18 +1,20 @@
 from pprint import pprint
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
+import os
 
 credentials = GoogleCredentials.get_application_default()
 service = discovery.build("sqladmin", "v1beta4", credentials=credentials)
+project_id = os.environ.get("GCP_PROJECT")
 
 def cloudsql_on(request):
-    project = "your-project-id" # TODO
+    activation_policy = "ALWAYS"
     instance_name = "instance-name" # TODO
 
-    dbinstancebody = {"settings": {"activationPolicy": "ALWAYS"}}
+    dbinstancebody = {"settings": {"activationPolicy": activation_policy}}
 
     request = service.instances().patch(
-        project=project, instance=instance_name, body=dbinstancebody
+        project=project_id, instance=instance_name, body=dbinstancebody
     )
     response = request.execute()
 
