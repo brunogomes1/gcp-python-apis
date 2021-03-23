f795575b2f7469cea9ba77e9b482cb8fc4cc7f68
brunogomes1 <80266185+brunogomes1@users.noreply.github.com>
2021-03-23 16:29:39 -0300

    adding cloudsql functions
    functions to turn on and turn off an specific CloudSQL instance

---
+++ cloudsql-api/turn_off/main.py
@@ -0,0 +1,19 @@
+from pprint import pprint
+from googleapiclient import discovery
+from oauth2client.client import GoogleCredentials
+
+credentials = GoogleCredentials.get_application_default()
+service = discovery.build("sqladmin", "v1beta4", credentials=credentials)
+
+def cloudsql_on(request):
+    project = "your-project-id" # TODO
+    instance_name = "instance-name" # TODO
+
+    dbinstancebody = {"settings": {"activationPolicy": "NEVER"}}
+
+    request = service.instances().patch(
+        project=project, instance=instance_name, body=dbinstancebody
+    )
+    response = request.execute()
+
+    pprint(response)
